abfea4b7f5a7300a2517c819ff69f930e1ff5b47
avyfain <avyfain@gmail.com>
2016-10-07 09:14:07 -0700

    old state, just recording it here

--- tagger.py
+++ tagger.py
@@ -1,24 +1,28 @@
 import os
 import ast
 import pocket
-from flask import Flask
-from apscheduler.schedulers.background import BackgroundScheduler
+# from flask import Flask, render_template
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
 TOKEN_PAIRS = os.environ.get('TOKENS')
-sched = BackgroundScheduler()
 
-sched.start()
+# @app.route('/tag')
+# def tag():
+#     names = tag_it_all()
+#     return "We've tagged your stuff: " + ', '.join(names)
 
-@app.route('/tag')
-def tag():
-    names = tag_it_all()
-    return "We've tagged your stuff: " + ', '.join(names)
+# @app.route('/')
+# def hello():
+#     return 'Hello World! This is our pocket tagger'
 
+# @app.route('/read')
+# def read():
+#     for pocketier in pocketiers:
+#         names.append(pocketier['name'])
+#     return render_template('read.html', name=name)
 
-@sched.scheduled_job('interval', minutes=60)
 def tag_it_all():
     pocketiers = ast.literal_eval(TOKEN_PAIRS)
     names = []
@@ -29,13 +33,8 @@ def tag_it_all():
         except (pocket.InvalidQueryException, pocket.AuthException):
             print(pocketier['token'], 'failed. Retrying...')
     return names
-    
 
-@app.route('/')
-def hello():
-    return 'Hello World! This is our pocket tagger'
-
-def tag_art(art, pocket_instance):
+def tag_art(art, pocket_client):
     try:
         w = int(art['word_count'])
         if w < 100: tag = "whoops"
@@ -44,19 +43,19 @@ def tag_art(art, pocket_instance):
         elif w < 1500: tag = "mid"
         elif w < 2000: tag = "mid-long"
         else: tag = "long"
-        pocket_instance = pocket_instance.tags_add(int(art['item_id']), tag)
+        pocket_client = pocket_client.tags_add(int(art['item_id']), tag)
     except KeyError:
         print('Missing')
-    return pocket_instance
+    return pocket_client
 
-def tag_user_articles(consumer_key, token, qty=100):
+def tag_user_articles(consumer_key, token, qty=1000):
     client = pocket.Pocket(consumer_key, token)
 
     r = client.get(state='all',count = qty)
     art_list = r[0]['list']
 
     count = 0
-    visited = {}
+    visited = set()
 
     for art_id, art in art_list.items():
         count += 1
@@ -68,11 +67,29 @@ def tag_user_articles(consumer_key, token, qty=100):
         try:
             if art['resolved_title'] in visited or art['resolved_url'] in visited:
                 continue
-            visited[art['resolved_title']] = True
-            visited[art['resolved_url']] = True
+            visited.add(art['resolved_title'])
+            visited.add(art['resolved_url'])
         except:
             print('Fail')
         client.tags_clear(art_id)
         tag_art(art, client)
 
     client.commit()
+
+# def get_union():
+#     pocketiers = ast.literal_eval(TOKEN_PAIRS)
+#     for pocketier in pocketiers:
+#         client = pocket.Pocket(CONSUMER_KEY, pocketier['token'])
+#         data, headers = client.get(count=1000)
+#         try: 
+#             union = union & set(data['list'].keys())
+#         except:
+#             union = set(data['list'].keys())
+
+#     union_strings = []
+#     for item in union:
+#         union_strings.append(data['list'][item]['resolved_title'] +' '+ data['list'][item]['resolved_url'])
+#     return union_strings
+
+if __name__ == '__main__':
+    tag_it_all()    
